50964387a5b5220238ceaf098f3f1160c5214e9d
Daniil Netrebko <dnetrebko@yandex.ru>
2019-04-08 22:33:48 +0300

    Added search by user name and phones

--- assets/js/components/ContactList.vue
+++ assets/js/components/ContactList.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 	<!--{{contacts}}-->
-	<v-data-table :items="contacts" :headers="headers" class="elevation-1" :search="search" rows-per-page-text="Контактов на странице">
+	<v-data-table :items="contacts" :headers="headers" class="elevation-1" :search="search" :custom-filter="customFilter" rows-per-page-text="Контактов на странице">
 		<template slot="pageText" scope="{ pageStart, pageStop, itemsLength }">
 				С {{ pageStart }} по {{ pageStop }} из {{ itemsLength }} контактов
 		</template>	
@@ -89,7 +89,48 @@ export default {
 	methods: {
 		showAddContactForm: function() {
 			this.$root.$emit('showAddContactForm')
-		}
+		},
+		
+		customFilter(items, search, filter) {
+			//this custom filter will do a multi-match separated by a space.
+			//e.g
+
+			if (!search) { return items } //if the search is empty just return all the items
+
+			function  new_filter (val, search) {
+				search = search.toString().toLowerCase()
+			
+				return val !== null &&
+					['undefined', 'boolean'].indexOf(typeof val) === -1 &&
+					//val.toString().toLowerCase().replace(/[^0-9a-zA-ZА-Яа-яёЁ]+/g,"").indexOf(search) !== -1
+					val.toString().toLowerCase().indexOf(search) !== -1
+			}
+
+			//let needleAry = search.toString().toLowerCase().split(" ").filter(x => x);
+			//whenever we encounter a space in our search we will filter for both the first and second word (or third word)
+
+			//return items.filter(row => needleAry.every(needle => Object.keys(row).some(key => new_filter(row[key],needle))));
+			return items.filter(function(row) {
+				console.log(Object.values(row.phones));
+				console.log(Object.values(row.phones).map(x => x.phone));
+			
+				if (new_filter(row.name, search))
+					return true;
+				
+				
+				
+				if (Object.values(row.phones).map(x => x.phone).some(y => new_filter(y, search)))
+					return true;
+				
+				return false;
+			})
+			//foreach needle in our array cycle through the data (row[key]) in the current row looking for a match.
+			// .some will return true and exit the loop if it finds one it will return false if it doesnt
+			// .every will exit the loop if we dont find a match but will return true if all needles match
+			// .filter the rows on each match
+
+
+		}		
 	}
 }
 </script>
