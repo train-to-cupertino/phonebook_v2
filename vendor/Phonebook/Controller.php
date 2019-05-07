852e920cba7ca948e41ca87dca5b8154c50d5eb8
Daniil Netrebko <dnetrebko@yandex.ru>
2019-05-07 21:32:06 +0300

    Minor backend fixes

--- vendor/Phonebook/Controller.php
+++ vendor/Phonebook/Controller.php
@@ -16,10 +16,14 @@ class Controller {
 	];
 	
 	private $_defaultAction = 'index';
-	
+
+	/**
+		Выполняет действие контроллера
+	*/
 	public function run() {
 		$action = isset($_GET['action']) && !empty($_GET['action']) ? $_GET['action'] : $this->_defaultAction;
 	
+		// Если действие задано и находится в списке допустимых
 		if (!empty($action) && in_array($action, $this->_actions)) {
 			
 			$phonebook = new Phonebook();
@@ -28,130 +32,163 @@ class Controller {
 				// Список пользователей
 				case "list":
 					try {
-						$list = $phonebook->getContactList();
+						$list = $phonebook->getContactList(); // Получаем список пользователей
 					} catch(Exception $e) {
 						echo json_encode(['result' => 'error', 'text' => $e->getMessage()]);
+						die();
 					}
 					
 					echo json_encode(['result' => 'success', 'data' => $list]);
+					die();
 				break;
 				
 				// Добавить контакт
 				case "add_contact":
 					try  {
+						// Если заданы непустые имя контакта и телефон
 						if (isset($_GET['name']) && isset($_GET['phone']) && $_GET['name'] && $_GET['phone']) {
 							try {
-								//$contact_id = $phonebook->addContact($_GET['name'], $_GET['phone']);
-								$res = $phonebook->addContact($_GET['name'], $_GET['phone']);
-								//if (!$contact_id) {
+								$res = $phonebook->addContact($_GET['name'], $_GET['phone']);	// Добавляем контакт
+
 								if (!$res) {
 									echo json_encode(['result' => 'error', 'text' => 'Не удалось добавить контакт!']);
+									die();
 								}
 							} catch(Exception $e) {
 								echo json_encode(['result' => 'error', 'text' => $e->getMessage()]);
+								die();
 							}
 
+							// Вывод при успешном добавлении
 							echo json_encode(['result' => 'success', 'contact_id' => $res['contact_id'], 'phone_id' => $res['phone_id']]);
+							die();
 						} else {
 							echo json_encode(['result' => 'error', 'text' => 'Не указано имя и/или телефон!']);
+							die();
 						}
 					} catch(Exception $e) {
 						echo json_encode(['result' => 'error', 'text' => $e->getMessage()]);
+						die();
 					}
 				break;
 				
 				// Отредактировать контакт (сменить имя)
 				case "update_contact":
+					// Если задано непустое имя контакта и ID, больший нуля
 					if (isset($_GET['contact_id']) && isset($_GET['name']) && ($_GET['contact_id'] > 0) && $_GET['name']) {
 						try {
-							$update_res = $phonebook->updateContact($_GET['contact_id'], $_GET['name']);
+							$update_res = $phonebook->updateContact($_GET['contact_id'], $_GET['name']); // Обновляем имя контакта
 							if (!$update_res) {
 								echo json_encode(['result' => 'error', 'text' => 'Не удалось отредактировать контакт!']);	
+								die();
 							} else {
 								echo json_encode(['result' => 'success']);
+								die();
 							}
 						} catch(Exception $e) {
 							echo json_encode(['result' => 'error', 'text' => $e->getMessage()]);
+							die();
 						}
 					} else {
-						echo json_encode(['result' => 'error', 'text' => 'Не задан ID контакта и/или имя контакта!']);	
+						echo json_encode(['result' => 'error', 'text' => 'Не задан ID контакта и/или имя контакта!']);
+						die();
 					}
 				break;
 				
 				// Отредактировать телефон (сменить номер)
 				case "update_phone":
+					// Если задан непустой телефон и ID, больший нуля
 					if (isset($_GET['phone_id']) && isset($_GET['phone']) && ($_GET['phone_id'] > 0) && $_GET['phone']) {
 						try {
-							$update_res = $phonebook->updatePhone($_GET['phone_id'], $_GET['phone']);
+							$update_res = $phonebook->updatePhone($_GET['phone_id'], $_GET['phone']);	// Обновляем телефон
 							if (!$update_res) {
 								echo json_encode(['result' => 'error', 'text' => 'Не удалось отредактировать телефон!']);	
+								die();
 							} else {
 								echo json_encode(['result' => 'success']);
+								die();
 							}
 						} catch(Exception $e) {
 							echo json_encode(['result' => 'error', 'text' => $e->getMessage()]);
+							die();
 						}
 					} else {
 						echo json_encode(['result' => 'error', 'text' => 'Не задан ID телефона и/или номер телефона!']);	
+						die();
 					}
 				break;				
 				
 				// Удалить контакт
 				case "delete_contact":
+					// Если задан ID контакта, больший нуля
 					if (isset($_GET['contact_id']) && ($_GET['contact_id'] > 0)) {
 						try {
-							$delete_res = $phonebook->deleteContact($_GET['contact_id']);
+							$delete_res = $phonebook->deleteContact($_GET['contact_id']); // Удаляем контакт
 							if (!$delete_res) {
 								echo json_encode(['result' => 'error', 'text' => 'Не удалось удалить контакт!']);	
+								die();
 							} else {
 								echo json_encode(['result' => 'success']);
+								die();
 							}
 						} catch(Exception $e) {
 							echo json_encode(['result' => 'error', 'text' => $e->getMessage()]);
+							die();
 						}
 					} else {
 						echo json_encode(['result' => 'error', 'text' => 'Не задан ID контакта!']);	
+						die();
 					}
 				break;
 				
 				// Добавить телефон
 				case "add_phone":
+					// Если задан ID контакта, больший нуля и непустой телефон
 					if (isset($_GET['contact_id']) && ($_GET['contact_id'] > 0) && isset($_GET['phone']) && $_GET['phone']) {
 						try {
-							$addphone_res = $phonebook->addPhone($_GET['contact_id'], $_GET['phone']);
+							$addphone_res = $phonebook->addPhone($_GET['contact_id'], $_GET['phone']); // Добавляем телефон
 							if (!$addphone_res) {
 								echo json_encode(['result' => 'error', 'text' => 'Не удалось добавить телефон!']);	
+								die();
 							} else {
 								echo json_encode(['result' => 'success', 'phone_id' => $addphone_res]);
+								die();
 							}
 						} catch(Exception $e) {
 							echo json_encode(['result' => 'error', 'text' => $e->getMessage()]);
+							die();
 						}
 					} else {
 						echo json_encode(['result' => 'error', 'text' => 'Не задан ID контакта и/или телефон!']);	
+						die();
 					}					
 				break;
 				
 				// Удалить телефон
 				case "delete_phone":
+					// Если задан ID телефона, больший нуля
 					if (isset($_GET['phone_id']) && ($_GET['phone_id'] > 0)) {
 						try {
-							$delete_res = $phonebook->deletePhone($_GET['phone_id']);
+							$delete_res = $phonebook->deletePhone($_GET['phone_id']); // Удаляем телефон
 							if (!$delete_res) {
-								echo json_encode(['result' => 'error', 'text' => 'Не удалось удалить телефон!']);	
+								echo json_encode(['result' => 'error', 'text' => 'Не удалось удалить телефон!']);
+								die();
 							} else {
 								echo json_encode(['result' => 'success']);
+								die();
 							}
 						} catch(Exception $e) {
 							echo json_encode(['result' => 'error', 'text' => $e->getMessage()]);
+							die();
 						}
 					} else {
 						echo json_encode(['result' => 'error', 'text' => 'Не задан ID контакта!']);	
+						die();
 					}
 				break;
 				
 				case "index":
-					echo file_get_contents('views/main.php');
+					echo file_get_contents($_SERVER['DOCUMENT_ROOT'].'/index.php');
 				break;
 			}
 		} else {
--- vendor/Phonebook/Phonebook.php
+++ vendor/Phonebook/Phonebook.php
@@ -17,7 +17,6 @@ class Phonebook {
 			$contacts[$row['id']] = $row;
 			$contacts[$row['id']]['phones'] = (object)null;
 		}
-		//return $contacts;
 		
 		// Добавляем к ним телефоны
 		$stmt = $pdo->query('SELECT id, contact_id, phone FROM phone');
@@ -34,8 +33,6 @@ class Phonebook {
 				$contacts[$row['contact_id']]['phones'] = [];
 			}
 			
-			//$contacts[$row['contact_id']]['phones'][] = $row;
-			//$contacts[$row['contact_id']]['phones'][$row['id']] = $row;
 			$contacts[$row['contact_id']]['phones'][$row['id']] = ['id' => $row['id'], 'phone' => $row['phone']];
 		}		
 
@@ -51,6 +48,7 @@ class Phonebook {
 		$pdo = $dbh->connectToDB();
 		
 		try {
+			$pdo->beginTransaction();
 			$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 
 				// Добавляем контакт
@@ -66,12 +64,11 @@ class Phonebook {
 					$res = $stmt->execute();
 					$phone_id = $pdo->lastInsertId();
 					
-					//return $contact_id;
 					if ($res) {
+						$pdo->commit();
 						return ['contact_id' => $contact_id, 'phone_id' => $phone_id];
 					}
 				}
-
 		} catch (Exception $e) {
 			$pdo->rollBack();
 			throw new Exception($e->getMessage());
@@ -96,13 +93,22 @@ class Phonebook {
 			if ($row['name'] == $name) {
 				return true;
 			} else {
-				// Выполняем update
-				$stmt = $pdo->prepare('UPDATE contact SET name = :name WHERE id = :id');
-				$stmt->bindParam(':name', $name);
-				$stmt->bindParam(':id', $id);
-				$stmt->execute();
+				try {
+					$pdo->beginTransaction();
 				
-				return true;
+					// Выполняем update
+					$stmt = $pdo->prepare('UPDATE contact SET name = :name WHERE id = :id');
+					$stmt->bindParam(':name', $name);
+					$stmt->bindParam(':id', $id);
+					$stmt->execute();
+					
+					$pdo->commit();
+					
+					return true;
+				} catch (Exception $e) {
+					$pdo->rollBack();
+					throw new Exception($e->getMessage());
+				}
 			}
 		} else {
 			return false;
@@ -115,13 +121,22 @@ class Phonebook {
 		$dbh = new DBHelper();
 		$pdo = $dbh->connectToDB();
 		
-		$stmt = $pdo->prepare('DELETE FROM contact WHERE id = :id');
-		$stmt->bindParam(':id', $id);
-		$stmt->execute();
-		
-		$res = $stmt->rowCount();
+		try {
+			$pdo->beginTransaction();
+			
+			$stmt = $pdo->prepare('DELETE FROM contact WHERE id = :id');
+			$stmt->bindParam(':id', $id);
+			$stmt->execute();
+			
+			$res = $stmt->rowCount();
+			
+			$pdo->commit();
 
-		return $res;
+			return $res;
+		} catch (Exception $e) {
+			$pdo->rollBack();
+			throw new Exception($e->getMessage());
+		}
 	}
 	
 	
@@ -130,22 +145,31 @@ class Phonebook {
 		$dbh = new DBHelper();
 		$pdo = $dbh->connectToDB();
 		
-		// Проверяем существование контакта
-		$stmt = $pdo->prepare('SELECT count(*) as "count" FROM contact WHERE id = :id');
-		$stmt->bindParam(':id', $contact_id);
-		$stmt->execute();
-		$row = $stmt->fetch();
-		if ($row['count'] > 0) {
-			// При наличии контакта - добавляем телефон
-			$stmt = $pdo->prepare("INSERT INTO phone (contact_id, phone) VALUES (:contact_id, :phone)");
-			$stmt->bindParam(':contact_id', $contact_id);
-			$stmt->bindParam(':phone', $phone);
-			$res = $stmt->execute();
-			$phone_id = $pdo->lastInsertId();			
-			
-			return $phone_id;
-		} else {
-			return false;
+		try {
+			$pdo->beginTransaction();
+		
+			// Проверяем существование контакта
+			$stmt = $pdo->prepare('SELECT count(*) as "count" FROM contact WHERE id = :id');
+			$stmt->bindParam(':id', $contact_id);
+			$stmt->execute();
+			$row = $stmt->fetch();
+			if ($row['count'] > 0) {
+				// При наличии контакта - добавляем телефон
+				$stmt = $pdo->prepare("INSERT INTO phone (contact_id, phone) VALUES (:contact_id, :phone)");
+				$stmt->bindParam(':contact_id', $contact_id);
+				$stmt->bindParam(':phone', $phone);
+				$res = $stmt->execute();
+				$phone_id = $pdo->lastInsertId();
+
+				$pdo->commit();
+				
+				return $phone_id;
+			} else {
+				return false;
+			}
+		} catch (Exception $e) {
+			$pdo->rollBack();
+			throw new Exception($e->getMessage());
 		}
 	}
 	
@@ -155,26 +179,35 @@ class Phonebook {
 		$dbh = new DBHelper();
 		$pdo = $dbh->connectToDB();
 	
-		// Проверяем существование телефона
-		$stmt = $pdo->prepare('SELECT phone FROM phone WHERE id = :id');
-		$stmt->bindParam(':id', $id);
-		$stmt->execute();
-		
-		if ($row = $stmt->fetch()) {
-			// Если номер меняется на такой же, то запрос не выполняем
-			if ($row['phone'] == $phone) {
-				return true;
+		try {
+			$pdo->beginTransaction();
+			
+			// Проверяем существование телефона
+			$stmt = $pdo->prepare('SELECT phone FROM phone WHERE id = :id');
+			$stmt->bindParam(':id', $id);
+			$stmt->execute();
+			
+			if ($row = $stmt->fetch()) {
+				// Если номер меняется на такой же, то запрос не выполняем
+				if ($row['phone'] == $phone) {
+					return true;
+				} else {
+					// Выполняем update
+					$stmt = $pdo->prepare('UPDATE phone SET phone = :phone WHERE id = :id');
+					$stmt->bindParam(':phone', $phone);
+					$stmt->bindParam(':id', $id);
+					$stmt->execute();
+					
+					$pdo->commit();
+					
+					return true;
+				}
 			} else {
-				// Выполняем update
-				$stmt = $pdo->prepare('UPDATE phone SET phone = :phone WHERE id = :id');
-				$stmt->bindParam(':phone', $phone);
-				$stmt->bindParam(':id', $id);
-				$stmt->execute();
-				
-				return true;
+				return false;
 			}
-		} else {
-			return false;
+		} catch (Exception $e) {
+			$pdo->rollBack();
+			throw new Exception($e->getMessage());
 		}
 	}	
 	
@@ -184,12 +217,21 @@ class Phonebook {
 		$dbh = new DBHelper();
 		$pdo = $dbh->connectToDB();
 		
-		$stmt = $pdo->prepare('DELETE FROM phone WHERE id = :id');
-		$stmt->bindParam(':id', $id);
-		$stmt->execute();
+		try {
+			$pdo->beginTransaction();
 		
-		$res = $stmt->rowCount();
+			$stmt = $pdo->prepare('DELETE FROM phone WHERE id = :id');
+			$stmt->bindParam(':id', $id);
+			$stmt->execute();
+			
+			$res = $stmt->rowCount();
+			
+			$pdo->commit();
 
-		return $res;
+			return $res;
+		} catch (Exception $e) {
+			$pdo->rollBack();
+			throw new Exception($e->getMessage());
+		}
 	}
 }
